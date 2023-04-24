cb5e9f2c24a6f958cdbf2494c1e5714c0f0b2d1f
Denis Belozerov <molotov.ant@mail.ru>
2023-04-24 22:02:44 +0500

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import { Container, Space } from "@mantine/core";
+
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <div className="App">
+    <Container fluid>
+      <Space h="md" />
       <Home />
-    </div>
+    </Container>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Input, Button, Space, Grid } from "@mantine/core";
+import { IconDeviceSpeaker } from "@tabler/icons-react";
 import { deezerRequestOptions } from "../helpers/requestOptions";
 
 export default function Home() {
+  const [query, setQuery] = useState("");
   const [tracks, setTracks] = useState<any[]>([]);
   const [audioSRC, setAudioSRC] = useState("");
 
@@ -11,12 +14,13 @@ export default function Home() {
   }, []);
 
   function requestMusic(q: string = "eminem") {
+    setAudioSRC("");
     async function getMusic(q: string = "eminem") {
       try {
         const response = await axios.request({ ...deezerRequestOptions, params: { q } });
         if (response.data.error) throw new Error();
         setTracks(response.data.data);
-        console.log(response.data.data);
+        console.log(response.data);
       } catch (error) {
         getMusic(q);
       }
@@ -26,12 +30,46 @@ export default function Home() {
 
   return (
     <div>
-      <audio controls autoPlay src={audioSRC} />
-      {tracks.map((e) => (
-        <div key={e.id} onMouseEnter={() => setAudioSRC(e.preview)}>
-          {e.title}
-        </div>
-      ))}
+      <Grid>
+        <Grid.Col span={11}>
+          <Input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            icon={<IconDeviceSpeaker />}
+            placeholder="eminem"
+            size="md"
+          />
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Button fullWidth onClick={() => requestMusic(query)} disabled={!query} size="md">
+            Найти
+          </Button>
+        </Grid.Col>
+      </Grid>
+
+      <Space h="md" />
+      <audio style={{ display: "none" }} controls autoPlay src={audioSRC} />
+      <div className="tracks">
+        {tracks.map((e) => (
+          <div
+            key={e.id}
+            className="track"
+            style={{ backgroundImage: `url("${e.album.cover_xl}")` }}
+            onMouseEnter={() => setAudioSRC(e.preview)}
+            onMouseLeave={() => setAudioSRC("")}
+          >
+            <div
+              className="track_artistName"
+              onClick={() => {
+                setQuery(e.artist.name);
+                requestMusic(e.artist.name);
+              }}
+            >
+              {e.artist.name}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
